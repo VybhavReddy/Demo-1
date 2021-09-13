ab42dc2b1b3f24a94ac118ce87cf0ee56ef92f71
Vybhav A <a.vybhav.reddy@gmail.com>
2021-09-13 19:00:25 +0530

    c# files added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter 2 integers");
+            int a= Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Addition value: {0}", a+b);
+            Console.WriteLine("subtraction value: {0}", a - b);
+            Console.WriteLine("Multiplication value: {0}", a * b);
+            Console.WriteLine("Division value: {0}", a / b);
+            Console.Read();
+        }
+    }
+}
