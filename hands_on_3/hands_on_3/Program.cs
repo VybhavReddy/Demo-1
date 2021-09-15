47eb30082292c9e17a76fab28f7a7a9b9de9937b
Vybhav A <a.vybhav.reddy@gmail.com>
2021-09-15 09:53:40 +0530

    hands on projects added

---
+++ hands_on_3/hands_on_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+namespace hands_on_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream read = new FileStream(@"C:\Users\Vybhav A\Desktop\sample.txt", FileMode.Open, FileAccess.Read);
+            StreamReader streamread = new StreamReader(read);
+            FileStream write = new FileStream(@"C:\Users\Vybhav A\Desktop\hands_on_3.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter streamwrite = new StreamWriter(write);
+            Console.WriteLine("Source       Destination     Date     Call");
+            string line = streamread.ReadLine();
+            while (streamread.Peek()>0)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (line.Contains(":"))
+                    {
+                        string[] s1 = line.Split(':');
+                        Console.Write(s1[1] + "   ");
+                        line = streamread.ReadLine();
+                    }
+                    else
+                    {
+                        line = streamread.ReadLine();
+                        break;
+                    }
+
+
+                }
+                Console.WriteLine();
+                
+            }
+            read.Close();
+            write.Close();
+            streamread.Close();
+        }
+    }
+}
